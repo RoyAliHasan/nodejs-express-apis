a729a26dec9b2c20428203e435b11b09aa520cfa
Roy Ali Hasan <royalihasan0987@gmail.com>
2023-01-25 23:14:27 +0500

    dom task

--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,23 @@
 var express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 // get user
+const JWT_SECRET = "bsf2206737";
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
 router.post(
-  "/createUser",
+  "/user",
   [
     body("name", "Enter a valid name").isLength({ min: 5 }),
     body("email", "enter valid email").isEmail(),
-    body("password", "password must be >8").isLength({ min: 8 }),
+    body("password", "password must be > 8").isLength({ min: 8 }),
   ],
   async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const secPassword = await bcrypt.hash(req.body.password, salt);
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -26,11 +32,17 @@ router.post(
     user = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      password: secPassword,
     });
-
-    res.send(user);
+    const data = {
+      id: user.id,
+    };
+    //
+    const authToken = jwt.sign(data, JWT_SECRET);
+    console.log(authToken);
+    res.send(authToken);
   }
 );
+  
 
 module.exports = router;
